9222edfaa931ed381fa0f63db70e9c4ccec84ec4
John Skjönsberg <JohnAtCristie@users.noreply.github.com>
2014-09-24 11:09:32 +0200

    Create init.pp

---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+#######################################################
+# Install TBMR from local RMP file
+# > Cristie Bare machine recovery for IBM Tivoli storage manager (TSM)
+# > is a disaster recovery software for quick restore
+# > to same or dissimilar hardware.
+#
+# http://www.cristie.com
+# http://www.cristie.se
+#
+# Version: 0.3
+# Author: John Skjönsberg <john.skjonsberg@cristie.se>
+# File: tbmr/manifests/init.pp
+#######################################################
+class tbmr(
+        $provider                       = $::tbmr::variables::provider,
+        $file                           = $::tbmr::variables::file,
+        $source                         = $::tbmr::variables::source,
+        ) inherits tbmr::variables {
+        validate_string($provider)
+        validate_string($file)
+        validate_string($source)
+        anchor {'tbmr::begin':} ->
+        class {'::tbmr::install':}
+        class {'::tbmr::license':}
+        anchor {'tbmr::end':}
+}
