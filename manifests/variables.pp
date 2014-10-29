b170d51191b03b06237233368e107576ce6aece0
JohnAtCristie <john.skjonsberg@cristie.se>
2014-10-29 19:47:21 +0100

    I take it all back...

--- manifests/variables.pp
+++ manifests/variables.pp
@@ -10,7 +10,7 @@
 #
 # http://www.cristie.se
 #
-# Version: 0.3
+# Version: 0.4
 #
 # Author: John Skjönsberg <john.skjonsberg@cristie.se>
 #
@@ -21,11 +21,11 @@ class tbmr::variables{
                         case $::hardwareisa{
                                 i386: {
                                         $file           = '/tmp/tbmr-7.1-2.i386.rpm'
-                                        $source         = 'puppet:///modules/puppet-tbmr/tbmr-7.1-2.i386.rpm'
+                                        $source         = 'puppet:///modules/tbmr/tbmr-7.1-2.i386.rpm'
                                 } #END 32bit
                                 x86_64: {
                                         $file           = '/tmp/tbmr-7.1-2.x86_64.rpm'
-                                        $source         = 'puppet:///modules/puppet-tbmr/tbmr-7.1-2.x86_64.rpm'
+                                        $source         = 'puppet:///modules/tbmr/tbmr-7.1-2.x86_64.rpm'
                                 }#END X86
                         }#END HARDWAREISA
                 } #END REDHAT
